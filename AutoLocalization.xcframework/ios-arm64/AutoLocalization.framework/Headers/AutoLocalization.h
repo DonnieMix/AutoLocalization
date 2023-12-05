baeed19b083de71857adf59d9f80c4cec6e1ab48
DonnieMix <kyrylo.derkach@ukma.edu.ua>
2023-12-05 18:57:06 +0200

    distribution changes for CocoaPods

---
+++ AutoLocalization.xcframework/ios-arm64/AutoLocalization.framework/Headers/AutoLocalization.h
@@ -0,0 +1,17 @@
+//
+//  AutoLocalization.h
+//  AutoLocalization
+//
+//  Created by Kyrylo Derkach on 29.11.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AutoLocalization.
+FOUNDATION_EXPORT double AutoLocalizationVersionNumber;
+
+//! Project version string for AutoLocalization.
+FOUNDATION_EXPORT const unsigned char AutoLocalizationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AutoLocalization/PublicHeader.h>
+
--- Podfile
+++ Podfile
@@ -9,3 +9,11 @@ target 'AutoLocalization' do
   # Pods for AutoLocalization
   pod 'GoogleMLKit/Translate', '3.2.0'
 end
+
+post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    target.build_configurations.each do |config|
+      config.build_settings["ONLY_ACTIVE_ARCH"] = "YES"
+    end
+  end
+end
